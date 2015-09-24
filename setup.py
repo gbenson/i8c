6d02b72ebf97f06ca640e16bd39338163e3a7402
Gary Benson <gbenson@redhat.com>
2015-09-24 10:19:33 +0100

    Enough setup.py to register with PyPI

--- setup.py
+++ setup.py
@@ -3,5 +3,13 @@ from setuptools import setup
 setup(
     name="i8c",
     version="0.0.1",
+    description="GNU Infinity note compiler",
+    #XXX long_description
+    #XXX and classifiers
+    #XXX see https://github.com/pypa/sampleproject/blob/master/setup.py
+    license="GPLv3+",
+    author="Gary Benson",
+    author_email="gbenson@redhat.com",
+    url="https://github.com/gbenson/i8c",
     packages=["i8c"],
     entry_points={"console_scripts": ["i8c = i8c:run_compiler"]})
