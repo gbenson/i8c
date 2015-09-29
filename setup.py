36134454c4cc1cb4d4abb6c1f1d838058e03160d
Gary Benson <gbenson@redhat.com>
2015-09-29 11:03:02 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,24 @@ setup(
     version="0.0.1",
     description="GNU Infinity note compiler",
     #XXX long_description
-    #XXX and classifiers
     #XXX see https://github.com/pypa/sampleproject/blob/master/setup.py
     license="GPLv3+",
     author="Gary Benson",
     author_email="gbenson@redhat.com",
     url="https://github.com/gbenson/i8c",
+    classifiers=[
+        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved" +
+            " :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Compilers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
     packages=["i8c"],
     entry_points={"console_scripts": ["i8c = i8c:run_compiler"]},
     tests_require=["nose"],
