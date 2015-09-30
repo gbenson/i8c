cfb7d50943a2d410d088b83b1d601305b6eb9283
Gary Benson <gbenson@redhat.com>
2015-09-30 20:47:11 +0100

    Check that empty functions can be compiled

---
+++ i8c/tests/test_empty_function.py
@@ -0,0 +1,9 @@
+from i8c.tests import TestCase
+
+SOURCE = "define test::pretty_minimal"
+
+class TestEmptyFunction(TestCase):
+    def test_empty_function(self):
+        """Check that empty functions can be compiled."""
+        tree, output = self.compile(SOURCE)
+        self.assertEqual([], output.operations)
