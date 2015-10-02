52484856bb43401434ad85d44e1a81bccdf4d50c
Gary Benson <gbenson@redhat.com>
2015-10-02 16:05:34 +0100

    Test synthetic return addition when last op is branch

---
+++ i8c/tests/test_last_op_branch.py
@@ -0,0 +1,25 @@
+from i8c.tests import TestCase
+
+SOURCE = """\
+define test::last_op_is_branch
+    argument bool x
+    argument bool y
+
+    goto label2
+label1:
+    return
+
+label2:
+    bne label1
+"""
+
+class TestFuncWithLastOpBra(TestCase):
+    def test_last_op_is_branch(self):
+        """Check that functions whose last op is a branch work.
+
+        This is testing the code that adds the synthetic return.
+        As a side-effect it also exercises the code that stops
+        us generating unnecessary gotos.
+        """
+        tree, output = self.compile(SOURCE)
+        self.assertEqual(["ne", "bra"], output.opnames)
